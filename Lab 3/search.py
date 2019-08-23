4e941dfaed7e15759eb348f4c3da24496c026781
Jatin Gandhi <45176848+jatin-gandhi@users.noreply.github.com>
2019-08-23 09:53:03 +0530

    Delete search.py

--- Lab 3/search.py
+++
@@ -1,9 +0,0 @@
-def search(li,key):
-	if key in li:
-		return True
-	else:
-		return False
-
-lis = list(map(int,input("Enter the elements in increasing order").split()))
-num = int(input("Enter the number to search"))
-print(search(lis,num))
